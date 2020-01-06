96b97d5e4503d9b4bd1cb072d4e2d3062175c494
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-01-06 13:22:00 +0100

    style

--- pageChanger.js
+++ pageChanger.js
@@ -1,11 +1,11 @@
 const fs = require("fs")
 
-module.exports = (pageName) => {
+module.exports = (pageName, data = {}) => {
     const app = document.getElementById("app")
     const newPageContent = fs.readFileSync("./pages/" + pageName + "/index.html",'utf8')
 
     app.innerHTML = ""
     app.innerHTML = newPageContent
     
-    require("./pages/" + pageName + "/index.js")()
+    require("./pages/" + pageName + "/index.js")(data)
 }
--- preload.js
+++ preload.js
@@ -1,8 +1,19 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const fs = require("fs")
+
 window.addEventListener('DOMContentLoaded', () => {
   const pageChanger = require("./pageChanger")
-  pageChanger("home")
+  const contents = fs.readFileSync("./data/data.json")
+  const gameData = JSON.parse(contents)
+  console.log(gameData)
 
-  document.getElementById("app").addEventListener("click", () => {pageChanger("test")})
+  const game = 0
+
+  var gameName = Object.keys(gameData[game])[0]
+  const question = gameData[game][gameName].questions[0]
+
+  pageChanger("ranking", {})
+
+  // document.getElementById("app").addEventListener("click", () => {pageChanger("test")})
 })
